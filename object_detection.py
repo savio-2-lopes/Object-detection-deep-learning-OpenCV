2140101a68df19f0da0d7eb9851acb47869e2553
Savio <60948849+savio-2-lopes@users.noreply.github.com>
2020-09-27 22:24:10 -0300

    Update object_detection.py

--- object_detection.py
+++ object_detection.py
@@ -37,19 +37,9 @@ detections = net.forward()
 
 for i in np.arange(0, detections.shape[2]):
 
-    # extract the confidence (i,e.. probability) associated with 
-    # predicition 
-
     confidence = detections[0,0,i,2] 
 
-    #filter out weak detections by ensuring the 'confidence' is 
-    # greater than the minimum confidence
-
     if confidence > args["confidence"]:
-
-        # Extract the index of the class label from the 'detection'
-        # then compute the (x, y) - coordinates of the bounding box 
-        # the object
 
         idx = int(detections[0,0,i,1])
         box = detections[0,0,i,3:7] * np.array([w, h, w, h])
@@ -62,9 +52,7 @@ for i in np.arange(0, detections.shape[2]):
         y = startY - 15 if startY - 15 > 15 else startY + 15
         cv2.putText(image, label, (startX, y), 
                     cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
-        
-        # Show the output image 
-        
+                
         cv2.imshow("Output", image)
         cv2.waitKey(0)
         
